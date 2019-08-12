b42b6ca6e950706af35b4ec6cef02eef7172522c
inu1255 <929909260@qq.com>
2019-08-12 13:01:34 +0800

    first

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import * as noginx from "express-noginx";
+import * as express from "express";
+import * as fs from "fs";
+import * as pdd from './pdd'
+import * as path from 'path'
+noginx.dir('cert')
+const proxy: express.Express = noginx.express();
+const app = express();
+
+proxy.use(function(req, res, next) {
+    if (req.method.toLowerCase() === "get") {
+        let filename = process.cwd() + '/sites/' + req.headers["host"] + req.path;
+        if (fs.existsSync(filename)) {
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.sendFile(filename)
+            return
+        }
+    }
+    next();
+});
+proxy.listen(9999, function() {
+    console.log(`app listen: ${9999}`)
+})
+
+app.get('*', function(req, res) {
+    if (req.query.anti_content) {
+        pdd.config.anti_content = req.query.anti_content;
+    }
+    res.end('');
+})
+
+app.listen(9998, function() {
+    console.log(`app listen: ${9998}`)
+})
+
+async function main() {
+    new pdd.Pdd('7198455919407', "ZK65GE5D2U3PXJWBMDS4UAPQO22XDKYWXPHE3CJZ2Y3GYQB3NZIA1032f82").start();
+    new pdd.Pdd('6187437120', "D4JQGQXXBIDPR5F4BNG5DOGF7ATFXZM4AMH6PBFI5DYSU5TWM6GQ10040a8").start();
+}
+
+main()
+
+process.on('uncaughtException', console.error);
